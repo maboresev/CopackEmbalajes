044851e7453cfeb62c34a114e6e375605cad94f6
xMastahx <themastah1234@gmail.com>
2017-04-05 14:48:41 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<style>
+button {
+	margin-right: 10px;
+}
+</style>
+<meta charset="utf-8">
+<form action="login_exito.html" method="post">
+Nickname:<br>
+<input type="text" name="nickname" required><br>
+Contraseña:<br>
+<input type="password" name="password" required=""><br><br>
+<button type="submit" name="login">Log in</button>
+<a href="register.html">Regístrate</a>
+</form>
+</html>
