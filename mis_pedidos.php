d8f899f73c7180073f64707ec7778b28f9d11531
xMastahx <themastah1234@gmail.com>
2017-05-26 23:41:58 +0200

    Pedidos done

--- mis_pedidos.php
+++ mis_pedidos.php
@@ -25,13 +25,17 @@
 	$conexion = crearConexionBD();
 
 		// La consulta que ha de paginarse
-	$query = 'SELECT PEDIDO.NUM_PEDIDO, PEDIDO.FECHA_PEDIDO, PEDIDO.OID_C, LINEA_DE_PEDIDO.OID_P, '
-		.'LINEA_DE_PEDIDO.NUM_PEDIDO, LINEA_DE_PEDIDO.CANTIDADPEDIDA, PRODUCTO.NOMBRE, PRODUCTO.OID_P '
-		.'FROM PEDIDO, LINEA_DE_PEDIDO, PRODUCTO '
-		.'WHERE '
-			.'LINEA_DE_PEDIDO.OID_P = PRODUCTO.OID_P AND '
-			.'PEDIDO.OID_P = LINEA_DE_PEDIDO.OID_P '
-		.'ORDER BY PEDIDO.NUM_PEDIDO';
+	$query = 'select PEDIDO.NUM_PEDIDO, PEDIDO.FECHA_PEDIDO,
+PRODUCTO.OID_P, PEDIDO.OID_C, PRODUCTO.NOMBRE,
+PRODUCTO.PRECIOUNITARIO, LINEA_DE_PEDIDO.CANTIDADPEDIDA
+
+
+from LINEA_DE_PEDIDO, PEDIDO, PRODUCTO
+
+where LINEA_DE_PEDIDO.NUM_PEDIDO = PEDIDO.NUM_PEDIDO and
+LINEA_DE_PEDIDO.OID_P = PRODUCTO.OID_P
+
+ORDER BY PEDIDO.NUM_PEDIDO';
 	
 		// Se comprueba que el tamaño de página, página seleccionada y total de registros son conformes.
 	// En caso de que no, se asume el tamaño de página propuesto, pero desde la página 1
@@ -65,7 +69,7 @@
 ?>
 
 <main>
-	 <nav>
+	 	 <nav>
 		<div class="enlaces">
 		<p class="textoGen">Páginas:</p>
 			<?php 
@@ -73,14 +77,18 @@
 					if ( $pagina == $pagina_seleccionada) { 	?>
 						<span class="current"><?php echo $pagina; ?></span>
 			<?php }	else { ?>
-						<a class="nocurrent" href="mis_pedidos.php?PAG_NUM=<?php echo $pagina; ?>&PAG_TAM=<?php echo $pag_tam; ?>"><?php echo $pagina; ?></a>
+						<a class="nocurrent" href="muestra_productos_cliente.php?PAG_NUM=<?php echo $pagina; ?>&PAG_TAM=<?php echo $pag_tam; ?>"><?php echo $pagina; ?></a>
 			<?php } ?>
 		</div>
 
-		<form method="get" action="mis_pedidos.php" class="consultaPaginada">
+		<form method="get" action="muestra_productos_cliente.php" class="consultaPaginada">
 			<input id="PAG_NUM" name="PAG_NUM" type="hidden" value="<?php echo $pagina_seleccionada?>"/>
-			Mostrando <?php echo $pag_tam ?> 
-			entrada de <?php echo $total_registros?>
+			Mostrando
+			<input id="PAG_TAM" name="PAG_TAM" type="number"
+				min="1" max="<?php echo $total_registros;?>"
+				value="<?php echo $pag_tam?>" autofocus="autofocus" />
+			entradas de <?php echo $total_registros?>
+			<input type="submit" value="Cambiar">
 		</form>  
 	</nav> 
 	
