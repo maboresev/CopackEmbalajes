3d9cd9cceaf38d74632cda73bbf380946ce27df5
Manuel Borrego <maboresev@gmail.com>
2017-06-22 11:29:43 +0200

    Completo

--- register.php
+++ register.php
@@ -34,6 +34,8 @@
 		apellidos=document.getElementById("apellidos").value;
 		email=document.getElementById("email").value;
 		empresa=document.getElementById("empresa").value;
+		var error1=passwordValidation();
+		var error2=passwordConfirmation();
 		
 		if((nombre == null) || (nombre.length == 0) || (/^\s+$/.test(nombre))){
 			alert("Por favor, escriba un nombre válido");
@@ -43,6 +45,10 @@
 		else if((apellidos == null) || (apellidos.length == 0) || (/^\s+$/.test(apellidos))){
 			alert("Por favor, escriba al menos un apellido");
 			document.getElementById("apellidos").style.borderColor="red";
+			return false;
+		}
+		else if((error1.length==0) && (error2.length==0)){
+			alert("Error en la contraseña.");
 			return false;
 		}
 		else if (!/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email)) {
@@ -59,6 +65,102 @@
 			return true;
 		}
 	}
+	
+	function passwordValidation(){
+		var password = document.getElementById("pass");
+		var pwd = password.value;
+		var valid = true;
+
+		// Comprobamos la longitud de la contraseña
+		valid = valid && (pwd.length>=8);
+		
+		// Comprobamos si contiene letras mayúsculas, minúsculas y números
+		var hasNumber = /\d/;
+		var hasUpperCases = /[A-Z]/;
+		var hasLowerCases = /[a-z]/;
+		valid = valid && (hasNumber.test(pwd)) && (hasUpperCases.test(pwd)) && (hasLowerCases.test(pwd));
+		
+		// Si no cumple las restricciones, devolvemos un error
+		if(!valid){
+			var error = "Please enter a valid password! Length >= 8, (upper and lower case) letters and digits";
+		}else{
+			var error = "";
+		}
+	        password.setCustomValidity(error);
+		return error;
+	}
+	
+	function passwordConfirmation(){
+		// Obtenemos el campo de password y su valor
+        var password = document.getElementById("pass");
+		var pwd = password.value;
+		// Obtenemos el campo de confirmación de password y su valor
+		var passconfirm = document.getElementById("confirmpass");
+		var confirmation = passconfirm.value;
+
+		// Los comparamos
+		if (pwd != confirmation) {
+			var error = "Please enter matching passwords!";
+		}else{
+			var error = "";
+		}
+
+		passconfirm.setCustomValidity(error);
+
+		return error;
+	}
+	
+	function passwordStrength(password){
+    		// Creamos un Map donde almacenar las ocurrencias de cada carácter
+    		var letters = {};
+
+    		// Recorremos la contraseña carácter por carácter
+    		var length = password.length;
+    		for(x = 0, length; x < length; x++) {
+        		// Consultamos el carácter en la posición x
+        		var l = password.charAt(x);
+
+        		// Si NO existe en el Map, inicializamos su contador a uno
+        		// Si ya existía, incrementamos el contador en uno
+        		letters[l] = (isNaN(letters[l])? 1 : letters[l] + 1);
+    		}
+
+    		// Devolvemos el cociente entre el número de caracteres únicos (las claves del Map)
+		// y la longitud de la contraseña
+    		return Object.keys(letters).length / length;
+	}
+	
+	function passwordColor(){
+		var passField = document.getElementById("password");
+		var strength = passwordStrength(passField.value);
+		
+		if(!isNaN(strength)){
+			var type = "weakpass";
+			if(passwordValidation()!=""){
+				type = "weakpass";
+			}else if(strength > 0.7){
+				type = "strongpass";
+			}else if(strength > 0.4){
+				type = "middlepass";
+			}
+		}else{
+			type = "nanpass";
+		}
+		passField.className = type;
+		
+		return type;
+	}
+	
+
+
+  </script>
+  
+  <script>
+	$(document).ready(function(){
+		$("#password").on("keyup", function(){
+			passwordColor();
+		});
+	});
   </script>
 </head>
 
